9ed978ab465a7a27c3a19856b4d85a690f4d4553
SadeepaNHerath <sadeepahearth@gmail.com>
2024-10-19 23:57:20 +0530

    Initial

---
+++ src/main/java/org/example/Starter.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/loginForm.fxml"))));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("img/logo.png")));
+        primaryStage.setTitle("TeeCeylon");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
